1c9fb154ab5c98040d6abad510249e461b1ef9f8
imufiid <imufiid98@gmail.com>
2021-04-12 09:20:59 +0700

    set autoload library prepocessing

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session','database','access','template');
+$autoload['libraries'] = array('session','database','access','template', 'Prepocessing', 'Filtering');
 
 /*
 | -------------------------------------------------------------------
