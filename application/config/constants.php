d7bf2f2b79b506434248ce7f58c8ca242a7f8b52
imufiid <imufiid98@gmail.com>
2021-04-12 20:48:16 +0700

    add constant value answer and answer key

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,13 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+
+/*
+|--------------------------------------------------------------------------
+| Type Sentence (Answer or Question)
+|--------------------------------------------------------------------------
+*/
+
+defined('ANSWER') OR define('ANSWER', 'answer');
+defined('KEY_ANSWER') OR define('KEY_ANSWER', 'key_answer');
