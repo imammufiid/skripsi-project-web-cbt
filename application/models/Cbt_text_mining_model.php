a5299fa86d0e0c50926de3b539ac55e064faaa52
imufiid <imufiid98@gmail.com>
2021-04-13 09:48:07 +0700

    create save batch function

--- application/models/Cbt_text_mining_model.php
+++ application/models/Cbt_text_mining_model.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Cbt_text_mining_model extends CI_Model
 {
-   public $table = 'cbt_tes_soal';
+   public $table = 'cbt_text_mining';
 
    function __construct()
    {
@@ -10,8 +10,12 @@ class Cbt_text_mining_model extends CI_Model
 
    function save($data)
    {
-      var_dump($data);die;
       $this->db->insert($this->table, $data);
       return $this->db->insert_id();
    }
+
+   function save_batch($data){
+      //$this->db->query($sql);
+      $this->db->insert_batch($this->table, $data);
+  }
 }
