e72836e70adfdb91414f437f5a4df2c24aa19d19
imufiid <imufiid98@gmail.com>
2021-04-13 10:38:46 +0700

    create function get test user and soal

--- application/models/Cbt_text_mining_model.php
+++ application/models/Cbt_text_mining_model.php
@@ -14,8 +14,26 @@ class Cbt_text_mining_model extends CI_Model
       return $this->db->insert_id();
    }
 
-   function save_batch($data){
+   function save_batch($data)
+   {
       //$this->db->query($sql);
       $this->db->insert_batch($this->table, $data);
+   }
+
+   function get_by_testuser_limit($tesuser_id, $limit)
+   {
+      $this->db->where('tm_tesuser_id="' . $tesuser_id . '"')
+         ->join('cbt_tes_soal', 'cbt_text_mining.tm_soal_id = cbt_tes_soal.tessoal_soal_id')
+         ->from($this->table)
+         ->limit($limit);
+      return $this->db->get();
+   }
+
+   function get_by_tessoal_limit($tm_tesuser_id, $limit){
+      $this->db->where('tm_tesuser_id="'.$tm_tesuser_id.'"')
+               ->join('cbt_soal', 'cbt_tes_soal.tessoal_soal_id = cbt_soal.soal_id')
+               ->from($this->table)
+               ->limit($limit);
+      return $this->db->get();
   }
 }
