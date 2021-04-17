58e09d8509be122053e56037972166ec15d6db3e
imufiid <imufiid98@gmail.com>
2021-04-17 14:20:20 +0700

    create term frecuency function

--- application/helpers/intersect_helper.php
+++ application/helpers/intersect_helper.php
@@ -8,3 +8,17 @@ if (!function_exists("intersect")) {
         return $countTermAnswerInDoc;
     }
 }
+
+if (!function_exists("term_frecuency")) {
+    function term_frecuency($arrayOfAnswer, $arrayOfkey) {
+        $intersect = array_intersect($arrayOfAnswer, $arrayOfkey); // hasil kata setelah disamakan dengan kata kunci jawaban
+        $intersectKey = array_intersect($arrayOfkey, $arrayOfkey); // hasil kata setelah disamakan dengan kata kunci jawaban
+        $tfAnswer = array_count_values($intersect); // menghitung jumlah kata per dokumen
+        $tfAnswerKey = array_count_values($intersectKey); // menghitung jumlah kata per dokumen
+
+        return [
+            "answer" => $tfAnswer,
+            "key"    => $tfAnswerKey
+        ];
+    }
+}
