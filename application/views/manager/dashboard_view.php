48cd1d51f86f6a865d404ddd521405c7675f5255
imufiid <imufiid98@gmail.com>
2021-04-21 09:29:25 +0700

    update dashboard ui

--- application/views/manager/dashboard_view.php
+++ application/views/manager/dashboard_view.php
@@ -1,25 +1,20 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
-	<h1>
-		<?php if(!empty($site_name)){ echo $site_name; } ?>
-	</h1>
-	<ol class="breadcrumb">
-		<li><a href="<?php echo site_url(); ?>/manager"><i class="fa fa-dashboard"></i> Home</a></li>
-		<li class="active">Dashboard</li>
-	</ol>
+    <h1>
+        <?php if (!empty($site_name)) echo $site_name; ?>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?php echo site_url(); ?>/manager"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+    </ol>
 </section>
 
 <!-- Main content -->
 <section class="content">
-    <div class="box box-default">
-        <div class="box-header with-border">
-            <h3 class="box-title">Tribute</h3>
-            <div class="box-tools pull-right">
-                <button class="btn btn-box-tool"><i class="fa fa-plus"></i></button>
-            </div><!-- /.box-tools -->
-        </div><!-- /.box-header -->
-        <div class="box-body">
-            <!-- Teruntuk Putri kami tercinta. Asyfiya Aniqa Putri, 28 Februari 2018 – 1 Maret 2018 -->
-        </div><!-- /.box-body -->
-    </div><!-- /.box -->
+    <div class="callout callout-info">
+        <h4>Informasi</h4>
+        <p>
+            Selamat datang di Halaman Dashboard Aplikasi <?php echo (!empty($site_name)) ? $site_name : "" ?>.
+        </p>
+    </div>
 </section><!-- /.content -->
