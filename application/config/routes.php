6884d37d7c00fdc283a1bfc8a8a7b1742afa973e
imufiid <imufiid98@gmail.com>
2021-08-16 20:17:14 +0700

    add route simple checker

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,3 +53,4 @@ $route['default_controller'] = 'welcome';
 $route['manager']="manager/welcome";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['checker'] = "manager/SimpleChecker";
